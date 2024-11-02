9de580650779123cb01f6778969e308f90050ed2
whitedevil2553 <ben10alienforce2553@gmail.com>
2024-11-02 21:18:03 +0530

    updated

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:44b35b82fa44e8b3a2b4b4c594e983b8de80aa534b939e60d8782ff6df37e036
-size 7842
+oid sha256:24e5aea6a667080eb2e0f9968dddba92db2955d34bd21098d0eeda1d849e2af6
+size 4257
