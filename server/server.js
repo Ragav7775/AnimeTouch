f6aa316b7563c706516fe18ec8029de155bd613b
whitedevil2553 <ben10alienforce2553@gmail.com>
2024-11-03 13:09:15 +0530

    server updated

--- server/server.js
+++ server/server.js
@@ -1,10 +1,200 @@
+// require('dotenv').config();
+// const express = require('express');
+// const bodyParser = require('body-parser');
+// const cors = require('cors');
+// const bcrypt = require('bcrypt');
+// const mongoose = require('mongoose');
+// const speakeasy = require('speakeasy');
+// const nodemailer = require('nodemailer');
+
+// const app = express();
+// const PORT = process.env.PORT || 5500;
+
+// // Connect to MongoDB Atlas
+// const mongoUri = process.env.MONGODB_URI;
+// if (!mongoUri) {
+//   throw new Error("MONGODB_URI environment variable not set");
+// }
+
+// mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true })
+//   .then(() => {
+//     console.log('Connected to MongoDB Atlas');
+//   })
+//   .catch((error) => {
+//     console.error('Error connecting to MongoDB Atlas:', error);
+//   });
+
+
+// const userSchema = new mongoose.Schema({
+//   username: { type: String, unique: true, required: true },
+//   email: { type: String, unique: true, required: true },
+//   password: { type: String, required: true },
+//   otp: String,
+//   otpExpiry: Date,
+// });
+
+// const User = mongoose.model('User', userSchema);
+
+// app.use(cors({
+//   origin: 'https://animetouch.vercel.app',  // client URL
+//   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+//   allowedHeaders: ['Content-Type', 'Authorization'],
+//   credentials: true
+// }));
+
+
+// app.use(bodyParser.json());
+
+
+// // Generate OTP
+// const generateOTP = () => {
+//   const otp = Math.floor(100000 + Math.random() * 900000).toString();
+//   console.log('Generated OTP:', otp);
+//   return otp
+// };
+
+// // Send OTP via email
+// const sendOTPEmail = (email, otp) => {
+//   const transporter = nodemailer.createTransport({
+//     service: 'gmail',
+//     host: 'smtp.gmail.com',
+//     port: 465, // or 465 if you're using SSL
+//     secure: false,
+//     auth: {
+//       user: process.env.EMAIL_USER,
+//       pass: process.env.EMAIL_PASS,
+//     },
+//   });
+
+//   const mailOptions = {
+//     from: process.env.EMAIL_USER,
+//     to: email,
+//     subject: 'Your OTP Code',
+//     text: `Your OTP code is ${otp}`,
+//   };
+
+//   transporter.sendMail(mailOptions, (error, info) => {
+//     if (error) {
+//       console.error('Error sending OTP email:', error);
+//       return; // Make sure it doesn't proceed when there's an error
+//     }
+//     console.log('OTP email sent:', info.response);
+//   });
+// };
+
+
+
+// app.post('/api/signup', async (req, res) => {
+//   const { username, email, password } = req.body;
+
+//   try {
+//     const existingUser = await User.findOne({ username });
+//     if (existingUser) {
+//       return res.json({ success: false, message: 'Username already taken' });
+//     }
+
+//     const hashedPassword = await bcrypt.hash(password, 10);
+//     const newUser = new User({ username, email, password: hashedPassword });
+//     await newUser.save();
+
+//     res.json({ success: true, message: 'Signup Successful' });
+//   } catch (error) {
+//     console.error('Error in signup:', error);
+//     res.status(500).json({ success: false, message: 'Internal Server Error' });
+//   }
+// });
+
+
+// app.post('/api/login', async (req, res) => {
+//   const { username, password } = req.body;
+
+//   try {
+//     // Check if user exists
+//     const user = await User.findOne({ username });
+//     if (user) {
+
+//       // Verify password
+//       const match = await bcrypt.compare(password, user.password);
+//       if (match) {
+
+//         // Generate OTP, save it in the user document, and send it via email
+//         const otp = generateOTP();
+//         user.otp = otp;
+//         user.otpExpiry = Date.now() + 10 * 60 * 1000; // 10 minutes from now
+//         await user.save();
+//         sendOTPEmail(user.email, otp);
+//       } else {
+//         return res.json({ success: false, message: 'Invalid password' });
+//       }
+
+//       res.json({ success: true, message: 'OTP sent to your email' });
+//     } else {
+//       return res.json({ success: false, message: 'Invalid username' });
+//     }
+//   } catch (error) {
+//     console.error('Error in login:', error);
+//     res.status(500).json({ success: false, message: 'Internal Server Error' });
+//   }
+// });
+
+
+// app.post('/api/verify-otp', async (req, res) => {
+//   const { username, otp } = req.body;
+
+//   try {
+//     const user = await User.findOne({ username });
+//     if (user) {
+//       const isValidOtp = speakeasy.totp.verify({
+//         secret: process.env.OTP_SECRET || 'supersecret',
+//         encoding: 'base32',
+//         token: otp,
+//         window: 1,
+//       });
+
+//       // Check if OTP is valid and not expired
+//       if (isValidOtp && Date.now() < user.otpExpiry) {
+//         user.otp = null; // Clear OTP after successful verification
+//         user.otpExpiry = null;
+//         await user.save();
+
+//         return res.json({ success: true });
+//       } else {
+//         return res.json({ success: false, message: 'Invalid or expired OTP' });
+//       }
+//     } else {
+//       return res.json({ success: false, message: 'Invalid username' });
+//     }
+//   } catch (error) {
+//     console.error('Error in OTP verification:', error);
+//     res.status(500).json({ success: false, message: 'Internal Server Error' });
+//   }
+// });
+
+
+// // Endpoint to check if the server is running
+// app.get('/api/status', (req, res) => {
+//   res.json({ status: 'Server is Running' });
+// });
+
+// // Start server
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
+
+
+
+
+
+
+
+
+
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const bcrypt = require('bcrypt');
 const mongoose = require('mongoose');
-const speakeasy = require('speakeasy');
 const nodemailer = require('nodemailer');
 
 const app = express();
@@ -24,7 +214,6 @@ mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true })
     console.error('Error connecting to MongoDB Atlas:', error);
   });
 
-
 const userSchema = new mongoose.Schema({
   username: { type: String, unique: true, required: true },
   email: { type: String, unique: true, required: true },
@@ -33,7 +222,7 @@ const userSchema = new mongoose.Schema({
   otpExpiry: Date,
 });
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model('User ', userSchema);
 
 app.use(cors({
   origin: 'https://animetouch.vercel.app',  // client URL
@@ -42,24 +231,17 @@ app.use(cors({
   credentials: true
 }));
 
-
 app.use(bodyParser.json());
-
 
 // Generate OTP
 const generateOTP = () => {
-  const otp = Math.floor(100000 + Math.random() * 900000).toString();
-  console.log('Generated OTP:', otp);
-  return otp
+  return Math.floor(100000 + Math.random() * 900000).toString();
 };
 
 // Send OTP via email
 const sendOTPEmail = (email, otp) => {
   const transporter = nodemailer.createTransport({
     service: 'gmail',
-    host: 'smtp.gmail.com',
-    port: 465, // or 465 if you're using SSL
-    secure: false,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
@@ -76,13 +258,11 @@ const sendOTPEmail = (email, otp) => {
   transporter.sendMail(mailOptions, (error, info) => {
     if (error) {
       console.error('Error sending OTP email:', error);
-      return; // Make sure it doesn't proceed when there's an error
+      return;
     }
     console.log('OTP email sent:', info.response);
   });
 };
-
-
 
 app.post('/api/signup', async (req, res) => {
   const { username, email, password } = req.body;
@@ -104,20 +284,14 @@ app.post('/api/signup', async (req, res) => {
   }
 });
 
-
 app.post('/api/login', async (req, res) => {
   const { username, password } = req.body;
 
   try {
-    // Check if user exists
     const user = await User.findOne({ username });
     if (user) {
-
-      // Verify password
       const match = await bcrypt.compare(password, user.password);
       if (match) {
-
-        // Generate OTP, save it in the user document, and send it via email
         const otp = generateOTP();
         user.otp = otp;
         user.otpExpiry = Date.now() + 10 * 60 * 1000; // 10 minutes from now
@@ -137,22 +311,13 @@ app.post('/api/login', async (req, res) => {
   }
 });
 
-
 app.post('/api/verify-otp', async (req, res) => {
   const { username, otp } = req.body;
 
   try {
     const user = await User.findOne({ username });
     if (user) {
-      const isValidOtp = speakeasy.totp.verify({
-        secret: process.env.OTP_SECRET || 'supersecret',
-        encoding: 'base32',
-        token: otp,
-        window: 1,
-      });
-
-      // Check if OTP is valid and not expired
-      if (isValidOtp && Date.now() < user.otpExpiry) {
+      if (user.otp === otp && Date.now() < user.otpExpiry) {
         user.otp = null; // Clear OTP after successful verification
         user.otpExpiry = null;
         await user.save();
@@ -170,13 +335,10 @@ app.post('/api/verify-otp', async (req, res) => {
   }
 });
 
-
-// Endpoint to check if the server is running
 app.get('/api/status', (req, res) => {
   res.json({ status: 'Server is Running' });
 });
 
-// Start server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
