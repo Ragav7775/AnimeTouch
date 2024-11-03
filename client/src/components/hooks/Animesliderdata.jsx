49285390aaaa12bb70b0cee66c09f77a7945325f
whitedevil2553 <ben10alienforce2553@gmail.com>
2024-11-03 15:43:35 +0530

    Assets modification

--- client/src/components/hooks/Animesliderdata.jsx
+++ client/src/components/hooks/Animesliderdata.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:14df8707df4762bf7ab7bf6b8851133d5a7c800e9cd24588e5a75590e01d5bbe
+oid sha256:233b058962c12139a57dfe0c1c5bca8d67e96843f224eb39ad0c7a9a4e2ae2ad
 size 76623
